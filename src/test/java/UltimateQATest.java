1712a060779137c98a2512e6c2f3710b15f91f93
KamilDrzewiecki023 <kamilslowik1996@gmail.com>
2023-04-30 15:08:54 +0200

    First Test Implemented

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UltimateQATest extends PageSetup {
+
+@Test
+    public void testOne(){
+
+    driver.findElementById("idExample").click();
+
+    WebElement ButtonSuccess = driver.findElementByClassName("entry-title");
+    Assertions.assertEquals("Button success", ButtonSuccess.getText());
+
+}
+
+
+
+
+    }
+
+
+
+
