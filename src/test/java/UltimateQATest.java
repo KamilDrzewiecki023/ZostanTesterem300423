0370b20993e6d1c34dfc33d363570f76a09bdfe8
KamilDrzewiecki023 <kamilslowik1996@gmail.com>
2023-04-30 16:50:04 +0200

    Second Test Implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -22,22 +22,32 @@ public class UltimateQATest extends PageSetup {
     @Test
     public void testTwo() throws InterruptedException {
 
-     WebElement nameInput = driver.findElementById("et_pb_contact_name_0");
-     nameInput.sendKeys("Tester");
+        WebElement nameInput = driver.findElementById("et_pb_contact_name_0");
+        nameInput.sendKeys("Tester");
 
-     WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
-      emailInput.sendKeys("tester@tester.pl");
+        WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
+        emailInput.sendKeys("tester@tester.pl");
 
-      Thread.sleep(2000);
+        Thread.sleep(2000);
 
-    WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
-    emailMeButton.click();
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
 
-    Thread.sleep(2000);
+        Thread.sleep(2000);
 
-    WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+        WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
 
-    Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
+        Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
+
+    }
+
+    @Test
+        public void testThree(){
+
+        WebElement blueField = driver.findElementByXPath("//*[contains(@class, 'et_pb_module et_pb_cta_0')]");
+        Assertions.assertEquals("rgba(46, 163, 242, 1)", blueField.getCssValue("background-color"));
+
+
 
 
     }
